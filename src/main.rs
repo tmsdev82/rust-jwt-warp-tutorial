a0f647ad052b6e7b0e6933dc9d61116ae40748c1
Tim Steeman <tmdev82@gmail.com>
2022-03-18 21:52:06 +0100

    implement basic server and add logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+use log::info;
+
+#[tokio::main]
+async fn main() {
+    log4rs::init_file("logconfig.yml", Default::default()).expect("Log config file not found.");
+    info!("Starting server...");
+    let root = warp::path::end().map(|| { "Welcome to the Rust REST API"});
+    
+    let routes = root.with(warp::cors().allow_any_origin());
+
+    warp::serve(routes).run(([127,0,0,1], 5000)).await;
 }
