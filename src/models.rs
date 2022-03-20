a8911eed232ed9fc03df52a9c0a203396a89a68e
Tim Steeman <tmdev82@gmail.com>
2022-03-20 15:51:53 +0100

    add model for login and claims

--- src/models.rs
+++ src/models.rs
@@ -14,3 +14,16 @@ pub struct CreateUser {
     pub password: String,
     pub role: String,
 }
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct LoginUser {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Claims {
+    pub sub: String,
+    pub role: String,
+    pub exp: usize,
+}
